ffca862fa41dcb2772f147b48edce275dbb65948
bbyba <burte.bsn@gmail.com>
2023-12-03 05:59:23 +0900

    Initial Commit

---
+++ Neobis_IOS_auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_IOS_auth
+//
+//  Created by Burte Bayaraa on 2023.12.03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
